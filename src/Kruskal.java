523885158c81c51048d0faf6ac61c645586e809f
Micaela Ramirez <ramirezmibe@gmail.com>
2017-06-30 10:49:46 -0300

    PriorityQueue en Kruskal

--- src/Kruskal.java
+++ src/Kruskal.java
@@ -1,5 +1,6 @@
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class Kruskal {
 	// In
@@ -15,30 +16,31 @@ public class Kruskal {
 	}
 	
 	public void run() {
-		LinkedList<NodoNodo> listaNodoNodo = new LinkedList<>();
+		PriorityQueue<NodoNodo> pq = new PriorityQueue<>(new Comparator<NodoNodo>() {
+			public int compare(NodoNodo o1, NodoNodo o2) { return o1.getCosto() - o2.getCosto(); }
+		});
+		
 		int n1, n2, c;
 		padre = new int[g.getSize()];
 		arbol = new GrafoPonderado(g.getSize());
 		
-		// Se ordenan los costos de las aristas
+		// Se cargan los costos de las aristas
 		for(int i=0;i<g.getSize();i++)
-			for(int j=0;j<g.getSize();j++) {
-				if(g.costo(i, j) != -1)
-					listaNodoNodo.add(new NodoNodo(i,j,g.costo(i, j))); // Si es digrafo, debería cargarlos una sola vez?
+			for(int j=i;j<g.getSize();j++) { // Recorro solo la mitad para no repetir aristas (usado para grafos no dirigidos)
+				if(g.costo(i, j) != -1) {
+					pq.offer(new NodoNodo(i,j,g.costo(i, j)));
+				}
 			}
-		listaNodoNodo.sort(new Comparator<NodoNodo>() {
-			@Override
-			public int compare(NodoNodo n1, NodoNodo n2) {
-				return n1.getCosto() - n2.getCosto();
-			}
-		});
 		
 		// En principio cada nodo es referente de sí mismo
 		for (int i = 0; i < padre.length; i++) {
 			padre[i] = i;
 		}
 		
-		for (NodoNodo n : listaNodoNodo) {
+		NodoNodo n;
+		while(!pq.isEmpty()) {
+			n = pq.poll(); // Saco de la cola al elemento del frente (sería el de menor costo)
+			
 			n1 = n.getNodo1();
 			n2 = n.getNodo2();
 			c = n.getCosto();
@@ -46,11 +48,10 @@ public class Kruskal {
 			if(!cierroCiclo(n1, n2)) {
 				costo += c; // Incremento costo total del arbol
 				arbol.conectar(n1, n2, c); // Agrego al arbol
-				union(n1, n2);
-				System.out.println("origen:"+(n1+1)+";dest:"+(n2+1));
+				union(n1, n2); // Cambio el referente
+				System.out.println("arco:"+(n1+1)+"-"+(n2+1));
 			}
 		}
-		
 		// si cant nodos - 1 es distinto a la cantidad de aristas, el grafo era no conexo
 	}
 	
